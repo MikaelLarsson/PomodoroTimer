0053e976ea1d1a5e26a532ca622220fc22a733f5
Mikael Larsson <mikael.larsson@hm.com>
2019-04-01 16:23:57 +0200

    Counting down seconds

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
+const defaultState = {
+    hours: 0,
+    minutes: 25,
+    seconds: 5
+}
+
 class Timer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            hours: 0,
-            minutes: 0,
-            seconds: 0
+        this.state = defaultState;
+    }
+    componentDidMount() {
+        this.timerId = setInterval(this.countDown, 1000);
+    }
+    countDown = () => {
+        if(this.state.seconds > 0) {
+            const seconds = this.state.seconds - 1;
+            this.setState({ seconds });
+        } else {
+            // clearInterval(this.timerId);
+            this.setState(defaultState);
         }
     }
     render() {
         const { hours, minutes, seconds } = this.state;
-        const time = `${hours}:${minutes}:${seconds}`;
+        const clock = `${hours}:${minutes}:${seconds}`;
         return (
-            <p>{ time }</p>
+            <p>{ clock }</p>
         );
     }
 }
