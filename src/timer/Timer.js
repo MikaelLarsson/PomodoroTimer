0df7b6fbb892ad3b729106df7f0603814cf1b466
Mikael Larsson <mikael.larsson@hm.com>
2019-04-26 16:05:39 +0200

    Added Pause/Play/Reset buttons

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -4,46 +4,59 @@ class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            timerRunning: false,
             time: this.setTime()
         };
     }
     setTime() {
         const time = new Date();
         time.setHours(0);
-        time.setMinutes(25);
-        time.setSeconds(0);
+        time.setMinutes(0);
+        time.setSeconds(3);
         return time;
     }
     componentDidMount() {
-        this.pomodoro = setInterval(this.countDown, 1000);
+        this.startTimer();
     }
-    countDown = () => {
-        const time = this.state.time;
-        if (!this.counterIsFinished(time)) {
+    startTimer = () => {
+        this.pomodoro = setInterval(this.timer, 1000);
+        this.setState({ timerRunning: true });
+    }
+    timer = () => {
+        if (!this.counterIsFinished()) {
+            const time = this.state.time;
             time.setSeconds(time.getSeconds() -1);
             this.setState({ time });
         } else {
             this.resetTime();
-            clearInterval(this.pomodoro);
-        }
-    }
-    counterIsFinished = time => {
-        const hours = time.getHours();
-        const minutes = time.getMinutes();
-        const seconds = time.getSeconds();
-        if (seconds === 0 && minutes === 0 && hours === 0) {
-            return true;
+            
         }
     }
     resetTime = () => {
         const time = this.setTime();
         this.setState({ time });
+        this.pauseTimer();
+    }
+    pauseTimer = () => {
+        clearInterval(this.pomodoro);
+        this.setState({ timerRunning: false });
+    }
+    counterIsFinished = () => {
+        const hours = this.state.time.getHours();
+        const minutes = this.state.time.getMinutes();
+        const seconds = this.state.time.getSeconds();
+        return seconds === 0 && minutes === 0 && hours === 0;
     }
     render() {
         const time = this.state.time;
-        const clock = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+        const clock = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
         return (
-            <p>{ clock }</p>
+            <div>
+                <p>{ clock }</p>
+                <button onClick={this.pauseTimer} disabled={!this.state.timerRunning}>Pause</button>&nbsp;
+                <button onClick={this.startTimer} disabled={this.state.timerRunning}>Play</button>&nbsp;
+                <button onClick={this.resetTime}>Reset</button>
+            </div>
         );
     }
 }
